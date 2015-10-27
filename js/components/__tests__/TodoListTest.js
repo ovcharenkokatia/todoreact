78a9d95f3e916631697372180c1501afd3503e8b
katiaOvcharenko <ovcharenko.katia@gmail.com>
2015-10-27 16:51:02 +0200

    added unit tests

--- js/components/__tests__/TodoListTest.js
+++ js/components/__tests__/TodoListTest.js
@@ -8,6 +8,11 @@ describe("TodoList", () => {
           todos={[ {id: 2, value: "u9i8", completed: false} ]}
        />);
 
+  it('should render one ul-element and one pre', () => {
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithTag(listComponent, 'ul').length).toBe(1);
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithTag(listComponent, 'pre').length).toBe(1);
+  });
+
   it("should render one todo item by default", () => {
     expect(listComponent.props.todos.length).toBe(1)
   });

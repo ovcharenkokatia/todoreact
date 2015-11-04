26029a497fbfea9503e949c5aa20f18d7e1fe346
katiaOvcharenko <ovcharenko.katia@gmail.com>
2015-11-04 10:07:01 +0200

    hot fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {render} from 'react-dom';
 import App from './src/client/app.js';
 
-React.render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
